5fa6fc2f8cec8a49c687a48cce125234cc84592f
LAPTOP-DPS9GSGG\admin <637640@sv.vnua.edu.vn>
2022-06-01 14:43:52 +0700

    feat: update

---
+++ ServerAPI/Server/Server/Models/NewFeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class NewFeed
+    {
+        [Key]
+        public string id { get; set; }
+        public string content { get; set; }
+        public DateTime datetimePost { get; set; }
+        public string idAccount { get; set; }
+    }
+}
